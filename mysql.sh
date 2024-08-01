f68633ddc95b7f5ade9af60c8b6ebd5b7fad53dd
ravirajdaws <raviraj.devops78@gmail.com>
2024-08-01 19:49:30 +0530

    setup root pwd

--- mysql.sh
+++ mysql.sh
@@ -12,21 +12,21 @@ N="\e[0m"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
-    then 
-    echo -e "$2 ....$R. failure  $N"
+   then 
+    echo -e "$2 ....$R failure $N"
     exit 1
-    else
-    echo -e "$2 ...$G.. success   $N"
+   else
+    echo -e "$2 ...$G success $N"
     fi
 }
 
-if [ $USERID -ne 0 ]
-  then 
+ if [ $USERID -ne 0 ]
+ then 
   echo "please try to run the script using root access"
   exit 1
-  else 
+ else 
   echo "you are a super user"
-fi
+ fi
 
   dnf install mysql-server -y &>>LOGFILE
   VALIDATE $? "installing mysql-server"
